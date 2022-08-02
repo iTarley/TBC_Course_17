daf513359286d5dd11c230a95ce294d831b29b26
Luka <nikuradzeluka0@gmail.com>
2022-08-02 14:39:35 +0400

    commit

--- app/src/main/java/com/example/tbc_course_17/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/example/tbc_course_17/ui/fragments/MainFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import com.example.tbc_course_17.R
+import com.example.tbc_course_17.databinding.FragmentMainBinding
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainFragment : Fragment() {
@@ -17,23 +20,39 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifecycleScope.launch(Dispatchers.IO){
+
+
+        lifecycleScope.launch(Dispatchers.Main){
+
             viewModel.getLastInformation()
+            viewModel.apiResponse.collect{
+                binding.message.text = it.toString()
+                binding.loading.visibility = View.INVISIBLE
+            }
         }
 
 
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/example/tbc_course_17/ui/fragments/MainViewModel.kt
+++ app/src/main/java/com/example/tbc_course_17/ui/fragments/MainViewModel.kt
@@ -1,26 +1,40 @@
 package com.example.tbc_course_17.ui.fragments
 
 import android.util.Log
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tbc_course_17.models.InfoModel
 import com.example.tbc_course_17.models.RetrofitClient
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
+    private val _apiResponse = MutableSharedFlow<InfoModel>()
+    val apiResponse:SharedFlow<InfoModel> get() = _apiResponse
+
     fun getLastInformation(){
 
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch {
             val response = RetrofitClient.getInformation().getInfo()
             if (response.isSuccessful){
                 val body:InfoModel? = response.body()
                 Log.d("response", "$body")
+                _apiResponse.emit(body!!)
             }else{
                 val error = response.errorBody()
                 Log.d("errorResponse", "$error")
             }
         }
+
     }
+
+
+
 }
